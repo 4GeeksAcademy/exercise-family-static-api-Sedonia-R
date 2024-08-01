eaab63b0c6d82e180365ed7c8045773e08ac6cdd
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-08-01 17:20:03 +0000

    updated to fix errors, but still need to work on the last assertion error

--- src/app.py
+++ src/app.py
@@ -14,6 +14,28 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+john = {
+    "first_name": "John",
+    "last_name": jackson_family.last_name,
+    "age": 33,
+    "lucky_numbers": [7, 13, 22],
+}
+jane = {
+    "first_name": "Jane",
+    "last_name": jackson_family.last_name,
+    "age": 35,
+    "lucky_numbers": [10, 14, 3],
+}
+jimmy = {
+    "first_name": "Jimmy",
+    "last_name": jackson_family.last_name,
+    "age": 5,
+    "lucky_numbers": [1],
+}
+
+jackson_family.add_member(john)
+jackson_family.add_member(jane)
+jackson_family.add_member(jimmy)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -26,17 +48,33 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def handle_get_all():
+    family_members = jackson_family.get_all_members()
+    return jsonify(family_members), 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_get_one(id):
+    family_member = jackson_family.get_member(id)
+    return jsonify(family_member), 200
 
+@app.route('/member', methods=['POST'])
+def handle_add_new():
+    family_member = request.json
+    jackson_family.add_member(family_member)
+    if family_member is not None:
+        return jsonify({"Message": f"Success! New family member {family_member} added."}), 200
+    else:
+        return jsonify({"Error": "Hmmm... looks like that family member already exists."}), 404
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['DELETE'])
+def handle_delete(id):
+    family_member = jackson_family.get_member(id)
+ 
+    if family_member:
+        jackson_family.delete_member(id)
+        return jsonify({"Message": f"Success! Memeber {family_member} deleted.", "done": True}), 200
+    else:
+        return jsonify({"Error": "Delete Failed. No family member found with the provided ID.", "done": True}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
